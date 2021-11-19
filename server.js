5a3e330e797ad25ba25eaa8ea70abb894cb7ede4
Moussatef <otman.moussetaf@gmail.com>
2021-11-19 12:24:08 +0100

    edit server

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const history = require('connect-history-api-fallback');
 
 const app = express();
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+// const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
+// app.use(staticFileMiddleware);
+// app.use(history({
+//   disableDotRule: true,
+//   verbose: true
+// }));
+// app.use(staticFileMiddleware);
 
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.render(path.join(__dirname + '/dist/index.html'));
+// });
 
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+// var server = app.listen(process.env.PORT || 8080, function () {
+//   var port = server.address().port;
+//   console.log("App now running on port", port);
+// });
 
 // const path = require('path')
 // const app = express()
-// app.use('/', serveStatic(path.join(__dirname, '/dist')))
-// const port = process.env.PORT || 8080
-// app.listen(port)
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+const port = process.env.PORT || 8080
+app.listen(port)
