9c9656b1e29eba57ef616879e4cf16bc38b82327
Moussatef <otman.moussetaf@gmail.com>
2021-11-19 13:06:20 +0100

    edit server

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 const express = require('express');
 const serveStatic = require('serve-static')
 const path = require('path');
-const history = require('connect-history-api-fallback');
 
 const app = express();
 
-// const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
-
-// app.use(staticFileMiddleware);
-// app.use(history({
-//   disableDotRule: true,
-//   verbose: true
-// }));
-// app.use(staticFileMiddleware);
-
-// app.get('/', function (req, res) {
-//   res.render(path.join(__dirname + '/dist/index.html'));
-// });
-
-// var server = app.listen(process.env.PORT || 8080, function () {
-//   var port = server.address().port;
-//   console.log("App now running on port", port);
-// });
-
-// const path = require('path')
-// const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8080
 app.listen(port)
